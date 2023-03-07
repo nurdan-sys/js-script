a6c2e7f0936a5696394a9e16f02e98c5689ebfbe
nurdan-sys <nurdan.alpbek@gmail.com>
2023-03-06 20:36:49 -1100

    js-script

---
+++ script/script.js
@@ -0,0 +1,24 @@
+// let a = 4
+// let b = 55
+// console.log(a+b)
+// let a = 4
+// let b = 55
+// console.log(a*b)
+// let a = 4
+// let b= 55
+// console.log(a/b)
+// let a = 4
+// let b= 55
+// console.log(a%b)
+// let a = "hello "
+// let b= "Nurdan"
+// console.log(a+b)
+// let a = "hello"
+// let b= 4
+// console.log(a/b)
+// let a = 5
+// let b= "5"
+// console.log(+b+a)
+// let c = 2
+// let b = undefined
+// console.log(c + b)
